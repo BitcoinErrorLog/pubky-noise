060118f67cbc7bf5df3edb9ef37f09dcd5e92fea
John Carvalho <18273620+BitcoinErrorLog@users.noreply.github.com>
2025-11-11 10:10:47 +0000

    Refactor build_initiator_ik_direct for clarity

--- src/client.rs
+++ src/client.rs
@@ -12,6 +12,7 @@ pub struct NoiseClient<R: RingKeyProvider, P> {
     pub suite: String,
     pub now_unix: Option<u64>,
 }
+
 impl<R: RingKeyProvider, P> NoiseClient<R, P> {
     pub fn new_direct(kid: impl Into<String>, device_id: impl AsRef<[u8]>, ring: std::sync::Arc<R>) -> Self {
         Self {
@@ -24,12 +25,16 @@ impl<R: RingKeyProvider, P> NoiseClient<R, P> {
             now_unix: None,
         }
     }
+
     pub fn build_initiator_ik_direct(&self, server_static_pub: &[u8;32], epoch: u32, server_hint: Option<&str>)
         -> Result<(snow::HandshakeState, Vec<u8>, u32), NoiseError>
     {
         let mut builder = snow::Builder::new(self.suite.parse().map_err(|e| NoiseError::Other(e.to_string()))?);
+
         let hs = self.ring.with_device_x25519(&self.kid, &self.device_id, epoch, |x_sk: &Zeroizing<[u8;32]>| {
-            if !crate::kdf::shared_secret_nonzero(x_sk, server_static_pub) { return Err(NoiseError::InvalidPeerKey); }
+            if !crate::kdf::shared_secret_nonzero(x_sk, server_static_pub) {
+                return Err(NoiseError::InvalidPeerKey);
+            }
             builder.local_private_key(x_sk);
             builder.remote_public_key(server_static_pub);
             builder.prologue(&self.prologue);
@@ -45,13 +50,22 @@ impl<R: RingKeyProvider, P> NoiseClient<R, P> {
         let msg32 = make_binding_message(tag, &self.prologue, &ed_pub, &x_pk_arr, Some(server_static_pub), epoch, Role::Client, server_hint);
         let sig64 = self.ring.sign_ed25519(&self.kid, &msg32)?;
 
-        let payload = IdentityPayload { ed25519_pub: ed_pub, noise_x25519_pub: x_pk_arr, epoch, role: Role::Client, server_hint: server_hint.map(|s| s.to_string()), sig: sig64 };
+        let payload = IdentityPayload {
+            ed25519_pub: ed_pub,
+            noise_x25519_pub: x_pk_arr,
+            epoch,
+            role: Role::Client,
+            server_hint: server_hint.map(|s| s.to_string()),
+            sig: sig64,
+        };
+
         let payload_bytes = serde_json::to_vec(&payload)?;
         let mut out = vec![0u8; payload_bytes.len() + 128];
         let n = hs.write_message(&payload_bytes, &mut out)?;
         out.truncate(n);
         Ok((hs, out, epoch))
     }
+
     pub fn build_initiator_xx_tofu(&self, _server_hint: Option<&str>) -> Result<(snow::HandshakeState, Vec<u8>), NoiseError> {
         let suite = "Noise_XX_25519_ChaChaPoly_BLAKE2s";
         let mut builder = snow::Builder::new(suite.parse().map_err(|e| NoiseError::Other(e.to_string()))?);
