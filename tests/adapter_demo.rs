a017f59c209b5b44ec6fa8dbcae5b18949da150f
John Carvalho <18273620+BitcoinErrorLog@users.noreply.github.com>
2025-11-19 14:54:17 +0000

    Delete tests directory

--- tests/adapter_demo.rs
+++
@@ -1,9 +0,0 @@
-use pubky_noise::*;
-#[test]
-fn adapter_smoke_compiles() {
-    let ring_client = std::sync::Arc::new(pubky_noise::DummyRing::new([1u8;32], "kid"));
-    let ring_server = std::sync::Arc::new(pubky_noise::DummyRing::new([2u8;32], "kid"));
-    let _client = pubky_noise::NoiseClient::<_, ()>::new_direct("kid", b"devC", ring_client);
-    let _server = pubky_noise::NoiseServer::<_, ()>::new_direct("kid", b"devS", ring_server, 3);
-    assert!(true);
-}
